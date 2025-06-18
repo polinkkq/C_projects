dce677bb55e23a63746e26e586dc0b0bc6acc5a7
polinkkq <polina.kylugina1@gmail.com>
2025-06-18 03:13:59 +0300

    Update files4.c

--- files/files4.c
+++ files/files4.c
@@ -8,12 +8,13 @@ int main(){
 	FILE * fin = fopen("in.txt", "r");
 	char str[100];
 	char new_str[10000] = "";
-	char separator = ';';
+	char separator[] = ";";
 	while(fgets(str, 10000, fin) != NULL){
-		str[strcspn(str, "\n")] = separator; //заменить /n на разделитель
+		str[strcspn(str, "\n")] = '\0'; //убрать \n
+		if (new_str[0] != '\0') //если строка не пустая
+			strcat(new_str, separator);
 		strcat(new_str, str);
 	}
 	printf("%s", new_str);
 	fclose(fin);
 }
-
